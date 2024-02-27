dc0d5689cf9b64f01e49efcd5a15946a08106c8d
Naidix Coder <matiinfante@gmail.com>
2024-02-27 00:45:36 -0300

    Desafio 2 backend

---
+++ main.js
@@ -0,0 +1,72 @@
+const ProductManager = require('./productManager'); //Importo la clase
+
+const mainProductManager = new ProductManager("./products.json"); 
+
+// EJECUCION DE LA APP //
+runApp();
+async function runApp() {
+    await uploadProducts(); // Carga 6 productos.
+    await deleteProducts(); // Borra producto ID 4, 5 y 6
+    await noDescriptionProduct(); // Intenta cargar producto sin descripcion.
+    await repeatedCode(); // Intenta cargar producto con codigo repetido
+    await readProducts(); // Devuevle el array de productos de products.json
+    await searchID(); // Busca ID 3 (existente), 10 y 15 no existentes.
+    await changeProduct(); // Modifica el producto con ID 3. 
+}
+
+// Carga de productos
+// title, description, price, thumbnail, stock, code
+async function uploadProducts() {
+    console.log("CARGA DE PRODUCTOS")
+    await mainProductManager.addProduct("product-1", "desc-1", 1000, "../path1", 10, "codigo-1")
+    await mainProductManager.addProduct("product-2", "desc-2", 2000, "../path2", 10, "codigo-2")
+    await mainProductManager.addProduct("product-3", "desc-3", 3000, "../path3", 10, "codigo-3")
+    await mainProductManager.addProduct("product-4", "desc-4", 4000, "../path4", 10, "codigo-4")
+    await mainProductManager.addProduct("product-5", "desc-5", 5000, "../path5", 10, "codigo-5")
+    await mainProductManager.addProduct("product-6", "desc-6", 6000, "../path6", 10, "codigo-6")
+}
+
+async function deleteProducts() {
+    console.log("ELIMINANDO PRODUCTOS 4,5 Y 6:")
+    await mainProductManager.deleteProduct(4)
+    await mainProductManager.deleteProduct(5)
+    await mainProductManager.deleteProduct(6)
+}
+
+async function changeProduct(){
+    console.log("MODIFICANDO PRODUCTO 3...")
+    await mainProductManager.updateProduct(3, {
+        title: "newTitle",
+        description: "newDescription",
+        price: 1500,
+        thumbnail: "new/path",
+        stock: 20,
+    });
+}
+
+async function noDescriptionProduct() {
+    console.log("CARGA DE PRODUCTO QUE LE FALTA DESCRIPCION:")
+    await mainProductManager.addProduct("product-7", 7000, "../path7", 10, "codigo-7")
+}
+
+async function repeatedCode(){
+    console.log("CARGA DE PRODUCTO CON CODE REPETIDO:")
+    await mainProductManager.addProduct("product-1", "desc-1", 1000, "../path1", 10, "codigo-1")
+    await mainProductManager.addProduct("product-2", "desc-2", 1000, "../path2", 10, "codigo-2")
+    await mainProductManager.addProduct("product-3", "desc-3", 1000, "../path3", 10, "codigo-3")
+}
+
+async function readProducts(){
+    console.log("CARGA DE ARRAY DE PRODUCTOS: ")
+    console.log(await mainProductManager.getProducts());
+    console.log("...")
+}
+
+async function searchID() {
+    console.log("BUSQUEDA DEL ID 3:")
+    console.log(await mainProductManager.getProductById(3));
+    console.log(await mainProductManager.getProductById(10));
+    console.log(await mainProductManager.getProductById(15));
+    console.log("...")
+}
+
---
+++ productManager.js
@@ -0,0 +1,121 @@
+const fs = require("fs");
+
+class ProductManager {
+    
+    constructor(path) {
+        this.products = [];
+        this.path = path
+    }
+
+    async addProduct(title, description, price, thumbnail, stock, code) {
+
+        if (this.validateInputs(title, description, price, thumbnail, stock, code)) {
+            return;
+        }
+
+        const product = {
+            id: this.#getID(),
+            code: code,
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            stock: stock,
+        };
+
+        this.products.push(product);
+        await this.writeProductsJson(product);
+    }
+
+    validateInputs(title, description, price, thumbnail, stock, code) {
+        if (!title || !description || !price || !thumbnail || !stock || !code) {
+            console.error('Error: Todos los campos son obligatorios');
+            return true;
+        }
+    
+        if (this.products.some(prod => prod.code === code)) {
+            console.error(`Error: Ya existe un producto con el código: "${code}"`);
+            return true;
+        }
+    
+        return false;  // False ---> si no hay errores
+    }
+
+    #getID() {
+        if (this.products.length === 0) return 1;
+        return this.products[this.products.length - 1].id + 1;
+    }
+
+    async getProducts() {
+        try {
+            const readFile = await fs.promises.readFile(this.path , "utf-8");
+            const productsObj = JSON.parse(readFile)
+            return productsObj;
+        }catch (error){
+            console.error("Error al obtener productos" , error);
+            return;
+        }
+    }
+
+    async getProductById(productId) {
+        await fs.promises.readFile(this.path, 'utf8');
+        const myProduct = this.products.find((product) => product.id === productId);
+        if (myProduct) {
+            return myProduct;
+        }else{
+            return `Error: Producto con ID ${productId} no encontrado`;
+        }
+    }
+
+    async updateProduct(productId, updatedData) {
+        try {
+            const readFile = await fs.promises.readFile(this.path, 'utf-8');
+            this.products = JSON.parse(readFile);
+    
+            const index = this.products.findIndex(product => product.id === productId);
+    
+            if (index !== -1) {
+                this.products[index] = {
+                    ...this.products[index],
+                    ...updatedData,
+                };
+    
+                await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, '\t'));
+    
+                console.log(`Producto actualizado: ${JSON.stringify(this.products[index])}`);
+            } else {
+                console.error(`Error: Producto con ID ${productId} no encontrado para actualizar`);
+            }
+        } catch (error) {
+            console.error("Error al actualizar el producto: ", error);
+        }
+    }
+    
+
+    async writeProductsJson(product) {
+        try {
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, '\t'));
+            console.log(`Producto agregado: ${JSON.stringify(product)}`);
+        } catch (error) {
+            console.error("Error al guardar los productos: ", error);
+        }
+    }
+
+    async deleteProduct(productId) {
+        const index = this.products.findIndex(prod => prod.id === productId);
+    
+        if (index !== -1) {
+            const deletedProduct = this.products.splice(index, 1)[0];
+            try {
+                await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"));
+                console.log(`Producto eliminado: ${JSON.stringify(deletedProduct)}`);
+            } catch (error) {
+                console.error("Error al guardar los productos: ", error);
+            }
+        } else {
+            console.error(`Error: ID ${productId} no encontrado para eliminar`);
+        }
+    }
+}
+
+module.exports = ProductManager;
